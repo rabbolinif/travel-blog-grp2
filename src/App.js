817be34cda1d78d942058b06cbbc8770dd4a6ae2
Stelios Rusta <steliosrusta@gmail.com>
2022-03-17 17:55:30 +0100

    testing contentful

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import SingleArticle from "./stelios_comp/SingleArticle";
+
+const contentful = require("contentful");
+const client = contentful.createClient({
+  space: "h5ckac8m4bnj",
+  accessToken: "8QnCWCQ_AAFD1jgJtBYFzUeCK3qHz2zpZEhBrIb9oc4",
+});
 const App = () => {
   return (
-    <div> Hello there</div>
+    <>
+      <div> Hello there</div>
+      <SingleArticle />
+    </>
   );
-}
+};
 
 export default App;
+
+client.getEntry("fOxW4tZa5qhXH19BD3ap4").then(function (entry) {
+  // logs the entry metadata
+  console.log(entry.sys);
+
+  // logs the field with ID title
+  console.log(entry.fields);
+});
