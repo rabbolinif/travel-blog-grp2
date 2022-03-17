8e3a8c6372d797363cbc033ccb3433796125edc1
Rabs <rabbolinif@gmail.com>
2022-03-17 17:05:16 +0100

    Router scaffolding

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import {Routes, Route, Link} from 'react-router-dom';
+import Layout from './components/Layout';
+
 const App = () => {
   return (
-    <div> Hello there</div>
+    <Routes>
+      <Route path='/' element={<Layout />} >
+        <Route index element={<div>All Posts</div>} />
+        <Route path='new-post' element={<div>Create Post</div>} />
+        <Route path='about' element={<div>About</div>} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
